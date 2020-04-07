c5daeef4c752d9dcc9a3a834ff698f19267dd539
lucasbuttow <lucas_buttow@outlook.com>
2020-04-06 22:43:36 -0300

    Detectando Login

---
+++ src/handlers/LoginHandler.php
@@ -0,0 +1,30 @@
+<?php
+namespace src\handlers;
+
+use \src\models\User;
+
+class LoginHandler {
+
+    public static function checkLogin() {
+        if(!empty($_SESSION['token'])) {
+            $token = $_SESSION['token'];
+
+            $data = User::select()->where('token', $token)->one();
+            if(count($data) > 0) {
+
+                $loggedUser = new User();
+                // $loggedUser->setId($data['id']);
+                // $loggedUser->setEmail($data['email']);
+                // $loggedUser->setName($data['name']);
+                $loggedUser->id = $data['id'];
+                $loggedUser->email = $data['email'];
+                $loggedUser->name = $data['name'];
+
+                return $loggedUser;
+            }
+        }
+
+        return false;
+    }
+
+}
